175bbee223cfb4b214c50ee84e6f20ec186b06dd
Andrei_Orlov <Andrei1311@gmail.com>
2025-08-26 19:57:56 +0700

    добавил изменения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
 import { ContactComponent } from "./components/contact/contact.component";
 import { SkillsComponent } from "./components/skills/skills.component";
 import { ProjectsComponent } from "./components/projects/projects.component";
@@ -9,10 +9,14 @@ import { HeaderComponent } from "./components/header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent, SkillsComponent, ProjectsComponent, AboutComponent, HeaderComponent],
+  imports: [ContactComponent, SkillsComponent, ProjectsComponent, AboutComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+  
 })
 export class AppComponent {
   title = 'my-angular18-app';
 }
+const routes: Routes = [
+  { path: 'skills', component: SkillsComponent },
+];
